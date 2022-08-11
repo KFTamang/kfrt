28efb110a77d472fc661db63add7e30e07aaf33c
KFTamang <subsonic.detonation@gmail.com>
2022-08-12 01:21:41 +0900

    add ray and render background

--- main.cpp
+++ main.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
+
+color ray_color(const ray& r){
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 -t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 int main(void){
-    const int image_width = 256;
-    const int image_height = 256;
+    // image
+    const auto aspect_ratio = 16.0 / 9.0; 
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
+    // camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+
+    // render
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-    for(int i=0; i<image_height; i++){
-        std::cerr << "Scanline remaining:" << image_height - i << std::endl;
-        for(int j=0; j<image_width; j++){
-            auto r = double(i) / (image_width - 1);
-            auto b = double(j) / (image_height - 1);
-            auto g = 0.5;
-            color pixel_color(r, b, g);
+    for(int j=image_height-1; j>=0; j--){
+        std::cerr << "Scanline remaining:" << image_height - j << std::endl;
+        for(int i=image_width-1; i>=0; i--){
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
