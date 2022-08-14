5359a2d144a355c36cf0772919875ddef8b01435
KFTamang <subsonic.detonation@gmail.com>
2022-08-14 23:37:18 +0900

    add metal to material

--- main.cpp
+++ main.cpp
@@ -41,8 +41,12 @@ int main(void){
     auto world = HittableList();
     auto material_ground = make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
     auto material_center = make_shared<Lambertian>(Color(0.7, 0.3, 0.3));
-    world.add(make_shared<Sphere>(Point3(0, 0, -1), 0.5, material_center));
-    world.add(make_shared<Sphere>(Point3(0, -100.5, -1), 100, material_ground));
+    auto material_left   = make_shared<Metal>(Color(0.8, 0.8, 0.8));
+    auto material_right  = make_shared<Metal>(Color(0.4, 0.7, 0.3));
+    world.add(make_shared<Sphere>(Point3( 0,      0, -1), 0.5, material_center));
+    world.add(make_shared<Sphere>(Point3( 1,      0, -1), 0.5, material_right));
+    world.add(make_shared<Sphere>(Point3(-1,    0.5, -1), 0.5, material_left));
+    world.add(make_shared<Sphere>(Point3( 0, -100.5, -1), 100, material_ground));
 
     // render
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
--- material.h
+++ material.h
@@ -17,7 +17,7 @@ class Lambertian : public Material{
                 // catch degeneration of scatter direction
                 scatter_direction = rec.normal;
             }
-            scattered = Ray(rec.p, random_unit_vector() + rec.normal);
+            scattered = Ray(rec.p, scatter_direction);
             attenuation = albedo;
             return true;
         }
@@ -25,3 +25,23 @@ class Lambertian : public Material{
     private:
         Color albedo;
 };
+
+class Metal : public Material{
+    public:
+        Metal(const Color& a) : albedo(a){}
+        
+        virtual bool scatter(const Ray& incident, const HitRecord& rec, Color& attenuation, Ray& scattered) const override{
+            auto scatter_direction = reflect(incident.direction(), rec.normal);
+            if (scatter_direction.is_near_zero()){
+                // catch degeneration of scatter direction
+                scatter_direction = rec.normal;
+            }
+            scattered = Ray(rec.p, scatter_direction);
+            attenuation = albedo;
+            return (dot(scatter_direction, rec.normal) > 0);
+        }
+
+    private:
+        Color albedo;
+};
+
